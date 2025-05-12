5b2e0ceb85529d333365d747f3c2627dd6cc8b0a
Luca Di Bello <lucadibello@proton.me>
2025-05-12 14:58:43 +0200

    fixed build scripts: now using correct path + correct commands to startup the test

--- submission/part_4/environment/test_fixed.sh
+++ submission/part_4/environment/test_fixed.sh
@@ -6,7 +6,7 @@ source ./run_tmux_cve_test.sh
 
 # Run the test for the fixed version
 run_tmux_test \
-  "25cae5d8f01d0deb050243842ed5d967b3dc411" \
+  "a868bacb46e3c900530bed47a1c6f85b0fbe701c" \
   "fixed version (3.1c)" \
   "Expecting NO crash for the fixed version." \
   20 # 20s timeout
