a723d355797b82a3fbe7d77bf5014e2daed5bfe2
Luca Di Bello <lucadibello@proton.me>
2025-05-14 16:17:00 +0200

    fix: now PoC script uses right commit hash

--- submission/part_4/environment/test_fixed.sh
+++ submission/part_4/environment/test_fixed.sh
@@ -6,6 +6,6 @@ source ./run_tmux_cve_test.sh
 
 # Run the test for the fixed version
 run_tmux_test \
-  "25cae5d86f01d0deb050243842ed5d967b3dc411" \
-  "fixed version (3.1c)" \
+  "a868bacb46e3c900530bed47a1c6f85b0fbe701c" \
+  "fixed version (patched 3.1b)" \
   "no_crash"
