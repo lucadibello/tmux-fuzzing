580c254cc2d71584021a3f2f779610e13c63c578
Luca Di Bello <lucadibello@proton.me>
2025-05-14 16:34:32 +0200

    now applying patch for fix

--- submission/part_4/environment/test_fixed.sh
+++ submission/part_4/environment/test_fixed.sh
@@ -4,8 +4,13 @@ set -euo pipefail
 # Source the common test script
 source ./run_tmux_cve_test.sh
 
+# donwload the patch for tmux
+PATCH_URL="https://github.com/tmux/tmux/commit/a868bacb46e3c900530bed47a1c6f85b0fbe701c.diff"
+wget $PATCH_URL -O tmux.patch
+
 # Run the test for the fixed version
 run_tmux_test \
-  "a868bacb46e3c900530bed47a1c6f85b0fbe701c" \
+  "6a33a12798b2afeee6fb7bba74d86d628137921e" \
   "fixed version (patched 3.1b)" \
-  "no_crash"
+  "no_crash" \
+  "tmux.patch"
