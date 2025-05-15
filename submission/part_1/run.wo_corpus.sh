da0d19d7b415784d38c96c3a02b2e3d28cf79d26
Luca Di Bello <lucadibello@proton.me>
2025-05-15 13:11:59 +0200

    fixed path + removed old scripts

--- submission/part_1/run.wo_corpus.sh
+++ submission/part_1/run.wo_corpus.sh
@@ -9,8 +9,8 @@ export SANITIZER="address"
 export RUNTIME=$((4 * 60 * 60)) # 4 hours
 export LABEL="part1_wo_corpus"
 
-export PATCH_FILE="./part1/remove_seed_corpus.patch" # Patch to apply
-export APPLY_PATCH_TO="project"                      # "oss-fuzz" or "project" or "" (no patch)
+export PATCH_FILE="./part_1/remove_seed_corpus.patch" # Patch to apply
+export APPLY_PATCH_TO="project"                       # "oss-fuzz" or "project" or "" (no patch)
 
 export EXPORT_RESULTS="true"                                    # Export corpus and coverage after run
 export CORPUS_EXPORT_DIR_BASE="${ROOT_DIR:-$(pwd)}/experiments" # Dir where to store the corpus
